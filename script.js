d7b5eeab207a02d10e262badc7e069fba4aad3e3
ARACELIRAMIREZ1 <11591767-ARACELIRAMIREZ1@users.noreply.replit.com>
2023-05-31 13:29:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let buttons = document.querySelector(".buttons");
+let optionOne = document.querySelector(".option-one");
+let optionTwo = document.querySelector(".option-two");
+let optionThree = document.querySelector(".option-three");
+let optionFour = document.querySelector(".option-four");
+
+button.onclick = (function() {
+  let userInput = document.querySelector(".option-one").value;
+  let passInput = document.querySelector(".password").value;
+  let message = document.querySelector(".message");
+
+ if (optionOne === time < 10) {
+        jokerDiv.style.display = "block";
+    } else if (time ===  time >= 10) {
+ }
